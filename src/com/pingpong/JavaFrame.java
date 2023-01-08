5bf3b10f0e0f1ae95a5f414f18c2f367ae7654f3
mk006819 <112651854+mk006819@users.noreply.github.com>
2023-01-09 00:24:36 +0530

    Initial commit

---
+++ src/com/pingpong/JavaFrame.java
@@ -0,0 +1,21 @@
+package com.pingpong;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JavaFrame extends JFrame {
+    JavaPanel panel;
+
+    JavaFrame() {
+        panel = new JavaPanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
+
